a2bfc9d300214f5d077ae17f437b7d3e8d289848
Vojin <30846914+Vojka93@users.noreply.github.com>
2023-08-06 17:15:16 +0200

    html structure and scss styling for the main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div className='app'>App</div>
+  return (
+    <div className='app'>
+      <div className='wrapper'>
+        <div className='left'>
+          <div className='left-item menu'>menu</div>
+          <div className='left-item categories'>categories</div>
+          <div className='left-item recipes'>recipes</div>
+        </div>
+        <div className='right'>right</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
